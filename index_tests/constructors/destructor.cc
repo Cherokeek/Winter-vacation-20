86364f49f54d6ea21e54bb2b0f9148867a4ec4e8
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-08 19:00:33 +0000

    v1.1.3

---
+++ index_tests/constructors/destructor.cc
@@ -0,0 +1,14 @@
+class Foo {
+public:
+  Foo() {}
+  ~Foo() {};
+};
+
+void foo() {
+  Foo f;
+}
+
+// TODO: Support destructors (notice how the dtor has no usages listed).
+//        - check if variable is a pointer. if so, do *not* insert dtor
+//        - check if variable is normal type. if so, insert dtor
+//        -
