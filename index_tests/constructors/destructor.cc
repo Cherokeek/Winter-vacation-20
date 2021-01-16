cd0d67a21d684ab6302b0a8ee17b5f3c55316773
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-16 09:20:09 +0000

    added jquery

--- index_tests/constructors/destructor.cc
+++ index_tests/constructors/destructor.cc
@@ -34,4 +34,23 @@ OUTPUT:
       "derived": [],
       "uses": ["8:7-8:8|16676|-1"]
     }, {
-      "usr": 4259594
+      "usr": 4259594751088586730,
+      "detailed_name": "void foo()",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "7:6-7:9|7:1-9:2|2|-1",
+      "bases": [],
+      "vars": [1893354193220338759],
+      "callees": ["8:7-8:8|3385168158331140247|3|16676", "8:7-8:8|3385168158331140247|3|16676"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 7440261702884428359,
+      "detailed_name": "Foo::~Foo() noexcept",
+      "qual_name_offset": 0,
+      "short_name": "~Foo",
+      "spell":
