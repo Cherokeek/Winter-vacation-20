83d335e82cbe139ceaac653a757f760744e3ec5a
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-16 21:10:38 +0000

    Added License file

--- index_tests/constructors/invalid_reference.cc
+++ index_tests/constructors/invalid_reference.cc
@@ -11,4 +11,28 @@ EXTRA_FLAGS:
 OUTPUT:
 {
   "includes": [],
-  "skipped_ranges":
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 17319723337446061757,
+      "detailed_name": "Foo::Foo::Foo()",
+      "qual_name_offset": 0,
+      "short_name": "Foo",
+      "spell": "4:6-4:9|4:1-4:11|1026|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 9,
+      "parent_kind": 23,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 15041163540773201510,
+      "detailed_name": "struct Foo {}",
+      "qual_name_offset": 7,
+      "short_name": "Foo",
+      "spell": "1:8-1:11|1:1-1:14|2|-1",
+      "bases": [],
+      "funcs": [1731
