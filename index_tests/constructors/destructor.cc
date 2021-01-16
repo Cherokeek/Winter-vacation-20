37566282fa5b183c082994957e09c7d8e340e0a7
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-16 08:59:00 +0000

    update classpath

--- index_tests/constructors/destructor.cc
+++ index_tests/constructors/destructor.cc
@@ -11,4 +11,27 @@ void foo() {
 // TODO: Support destructors (notice how the dtor has no usages listed).
 //        - check if variable is a pointer. if so, do *not* insert dtor
 //        - check if variable is normal type. if so, insert dtor
-//        -
+//        - scan for statements that look like dtors in function def handler
+//        - figure out some way to support w/ unique_ptrs?
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 3385168158331140247,
+      "detailed_name": "Foo::Foo()",
+      "qual_name_offset": 0,
+      "short_name": "Foo",
+      "spell": "3:3-3:6|3:3-3:11|1026|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 9,
+      "parent_kind": 5,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": ["8:7-8:8|16676|-1"]
+    }, {
+      "usr": 4259594
