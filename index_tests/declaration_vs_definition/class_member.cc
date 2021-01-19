b6a8da1c45e5cf9078b1e9d62e71671b3237223d
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-19 10:22:39 +0000

    Some code cleanup.

--- index_tests/declaration_vs_definition/class_member.cc
+++ index_tests/declaration_vs_definition/class_member.cc
@@ -13,4 +13,28 @@ OUTPUT:
       "detailed_name": "",
       "qual_name_offset": 0,
       "short_name": "",
-      "bases"
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 0,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [9736582033442720743],
+      "uses": []
+    }, {
+      "usr": 15041163540773201510,
+      "detailed_name": "class Foo {}",
+      "qual_name_offset": 6,
+      "short_name": "Foo",
+      "spell": "1:7-1:10|1:1-3:2|2|-1",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [{
+          "L": 9736582033442720743,
+          "R": 0
+        }],
+      "alias_of": 0,
