1cac1b4b84c0008b3e25e94e889c119e2106dff4
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-01-19 09:15:35 +0000

    Syncing with master

---
+++ index_tests/declaration_vs_definition/class_member.cc
@@ -0,0 +1,16 @@
+class Foo {
+  int foo;
+};
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [],
+  "usr2type": [{
+      "usr": 53,
+      "detailed_name": "",
+      "qual_name_offset": 0,
+      "short_name": "",
+      "bases"
