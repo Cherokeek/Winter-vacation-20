67ee6a5943ab06e7c4a2cae0861817e0b1cd9c56
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-03-03 17:38:30 +0000

    Updated Romanian translations from Adi Roiban

---
+++ index_tests/function_declaration_definition.cc
@@ -0,0 +1,14 @@
+void foo();
+
+void foo() {}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 4259594751088586730,
+      "detailed_name": "void foo()",
+      "qual_name_offset": 5,
+      "short
