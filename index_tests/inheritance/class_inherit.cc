0d8d91308c431dbe426f21b21a396aaf068201a1
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-03-20 14:58:25 +0000

    tiny cleanups

--- index_tests/inheritance/class_inherit.cc
+++ index_tests/inheritance/class_inherit.cc
@@ -19,4 +19,29 @@ OUTPUT:
       "vars": [],
       "alias_of": 0,
       "kind": 5,
- 
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [10963370434658308541],
+      "instances": [],
+      "uses": ["2:24-2:30|2052|-1"]
+    }, {
+      "usr": 10963370434658308541,
+      "detailed_name": "class Derived : public Parent {}",
+      "qual_name_offset": 6,
+      "short_name": "Derived",
+      "spell": "2:7-2:14|2:1-2:33|2|-1",
+      "bases": [3866412049634585509],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 5,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [],
+      "uses": []
+    }],
+  "usr2var": []
+}
+*/
