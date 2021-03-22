8d15b0b5f0f9280a7ae233c33263a228f75f7e2e
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-03-22 08:18:43 +0000

    basic working version

--- index_tests/inheritance/function_override.cc
+++ index_tests/inheritance/function_override.cc
@@ -17,4 +17,29 @@ OUTPUT:
       "short_name": "foo",
       "spell": "5:8-5:11|5:3-5:25|5186|-1",
       "bases": [9948027785633571339],
-      "vars"
+      "vars": [],
+      "callees": [],
+      "kind": 6,
+      "parent_kind": 5,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 9948027785633571339,
+      "detailed_name": "virtual void Root::foo()",
+      "qual_name_offset": 13,
+      "short_name": "foo",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 6,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["2:16-2:19|2:3-2:21|1089|-1"],
+      "derived": [6666242542855173890],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 3897841498936210886,
+      "deta
