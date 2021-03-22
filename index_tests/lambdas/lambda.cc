4f62d3d84d9726dc6b03e433af5abc367edddc9f
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-03-22 15:36:19 +0000

    / ‘README.md’

--- index_tests/lambdas/lambda.cc
+++ index_tests/lambdas/lambda.cc
@@ -8,4 +8,28 @@ void foo() {
 
   dosomething(1);
   dosomething(1);
-  dos
+  dosomething(1);
+}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 4259594751088586730,
+      "detailed_name": "void foo()",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "1:6-1:9|1:1-12:2|2|-1",
+      "bases": [],
+      "vars": [12666114896600231317, 2981279427664991319],
+      "callees": ["9:14-9:15|17926497908620168464|3|16420", "10:14-10:15|17926497908620168464|3|16420", "11:14-11:15|17926497908620168464|3|16420"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 17926
