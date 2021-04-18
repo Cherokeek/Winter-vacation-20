8f300a98d4a067f77d91eeac4cdff8a06e05943e
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-04-18 16:49:46 +0000

    fixes #10

--- index_tests/method_definition.cc
+++ index_tests/method_definition.cc
@@ -8,4 +8,25 @@ void Foo::foo() const {}
 OUTPUT:
 {
   "includes": [],
-  "skippe
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 6446764306530590711,
+      "detailed_name": "void Foo::foo() const",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "5:11-5:14|5:1-5:25|1026|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 6,
+      "parent_kind": 5,
+      "storage": 0,
+      "declarations": ["2:8-2:11|2:3-2:19|1025|-1"],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 15041163540773201510,
+      "detailed_name": "class Foo {}",
+      "qual_name_offset": 6,
+      "short_nam
