79680db03ec0f65b932e1a8bb964d456f0d52938
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-05-05 15:18:12 +0000

    fix for IE

--- index_tests/method_inline_declaration.cc
+++ index_tests/method_inline_declaration.cc
@@ -1,8 +1,7 @@
-class Foo {
-  void foo() const;
-};
 
-void Foo::foo() const {}
+class Foo {
+  void foo() {}
+};
 
 /*
 OUTPUT:
@@ -10,18 +9,18 @@ OUTPUT:
   "includes": [],
   "skipped_ranges": [],
   "usr2func": [{
-      "usr": 6446764306530590711,
-      "detailed_name": "void Foo::foo() const",
+      "usr": 17922201480358737771,
+      "detailed_name": "void Foo::foo()",
       "qual_name_offset": 5,
       "short_name": "foo",
-      "spell": "5:11-5:14|5:1-5:25|1026|-1",
+      "spell": "2:8-2:11|2:3-2:16|1026|-1",
       "bases": [],
       "vars": [],
       "callees": [],
       "kind": 6,
       "parent_kind": 5,
       "storage": 0,
-      "declarations": ["2:8-2:11|2:3-2:19|1025|-1"],
+      "declarations": [],
       "derived": [],
       "uses": []
     }],
@@ -32,7 +31,7 @@ OUTPUT:
       "short_name": "Foo",
       "spell": "1:7-1:10|1:1-3:2|2|-1",
       "bases": [],
-      "funcs": [6446764306530590711],
+      "funcs": [17922201480358737771],
       "types": [],
       "vars": [],
       "alias_of": 0,
@@ -41,7 +40,7 @@ OUTPUT:
       "declarations": [],
       "derived": [],
       "instances": [],
-      "uses": ["5:6-5:9|4|-1"]
+      "uses": []
     }],
   "usr2var": []
 }
---
+++ index_tests/multi_file/funky_enum.cc
@@ -0,0 +1,15 @@
+enum Foo {
+#include "funky_enum.h"
+};
+
+/*
+// TODO: In the future try to have better support for types defined across
+// multiple files.
+
+OUTPUT: funky_enum.h
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [],
+  "usr2type": [],
+  "
