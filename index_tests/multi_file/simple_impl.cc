afb751ad4590a0ac9f18318a561e4bdc42950158
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-06-20 19:40:57 +0000

    Linting fix

--- index_tests/multi_file/simple_impl.cc
+++ index_tests/multi_file/simple_impl.cc
@@ -8,4 +8,32 @@ void impl() {
 OUTPUT: simple_header.h
 {
   "includes": [],
-  "skipp
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 16236105532929924676,
+      "detailed_name": "void header()",
+      "qual_name_offset": 5,
+      "short_name": "header",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["3:6-3:12|3:1-3:14|1|-1"],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [],
+  "usr2var": []
+}
+OUTPUT: simple_impl.cc
+{
+  "includes": [{
+      "line": 0,
+      "resolved_path": "&simple_header.h"
+    }],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 3373269392705484958,
+      "detailed
