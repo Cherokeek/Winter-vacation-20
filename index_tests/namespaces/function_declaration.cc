d14e35018df8fd6405f6e44ca4cd9e53c472269e
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-06-24 10:55:38 +0000

    Add flake8 config

--- index_tests/namespaces/function_declaration.cc
+++ index_tests/namespaces/function_declaration.cc
@@ -16,4 +16,29 @@ OUTPUT:
       "vars": [],
       "callees": [],
       "kind": 12,
-      "parent_kind"
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["2:6-2:9|2:1-2:23|1025|-1"],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 2029211996748007610,
+      "detailed_name": "namespace hello {}",
+      "qual_name_offset": 10,
+      "short_name": "hello",
+      "bases": [],
+      "funcs": [18343102288837190527],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 3,
+      "parent_kind": 0,
+      "declarations": ["1:11-1:16|1:1-3:2|1|-1"],
+      "derived": [],
+      "instances": [],
+      "uses": []
+    }],
+  "usr2var": []
+}
+*/
---
+++ index_tests/namespaces/function_definition.cc
@@ -0,0 +1,28 @@
+
+namespace hello {
+void foo() {}
+}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 243328841292951622,
+      "detailed_name": "void hello::foo()",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "2:6-2:9|2:1-2:14|1026|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 3,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 2029211996748007610,
