bf7d500df372f4b2881e7a1e434cac5c845d2719
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-07-05 18:36:10 +0000

    virtio: rng: disallow multiple device registrations, fixes crashes

--- index_tests/objective-c/class.m
+++ index_tests/objective-c/class.m
@@ -2,4 +2,37 @@
   + (void)test;
   - (void)anInstanceMethod;
   @property (nonatomic) int aProp;
-@en
+@end
+
+@implementation AClass
++ (void)test {}
+- (void)anInstanceMethod {}
+@end
+
+int main(void)
+{
+  AClass *instance = [AClass init];
+  [instance anInstanceMethod];
+  instance.aProp = 12;
+}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_by_preprocessor": [],
+  "types": [{
+      "id": 0,
+      "usr": 11832280568361305387,
+      "detailed_name": "AClass",
+      "short_name": "AClass",
+      "kind": 7,
+      "spell": "7:17-7:23|-1|1|2",
+      "extent": "7:1-10:2|-1|1|0",
+      "parents": [],
+      "derived": [],
+      "types": [],
+      "funcs": [],
+      "vars": [],
+      "instances": [2],
+      "uses": 
