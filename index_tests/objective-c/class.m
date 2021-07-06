21eec6b9aa4a89a47db0ee8b104084365796b7e7
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-07-06 05:29:46 +0000

    Bump to version 0.1.0

--- index_tests/objective-c/class.m
+++ index_tests/objective-c/class.m
@@ -60,4 +60,25 @@ OUTPUT:
       "declarations": [{
           "spelling": "2:11-2:15",
           "extent": "2:3-2:16",
-          "c
+          "content": "+ (void)test;",
+          "param_spellings": []
+        }],
+      "spell": "8:9-8:13|-1|1|2",
+      "extent": "8:1-8:16|-1|1|0",
+      "base": [],
+      "derived": [],
+      "locals": [],
+      "uses": [],
+      "callees": []
+    }, {
+      "id": 1,
+      "usr": 4096877434426330804,
+      "detailed_name": "AClass::anInstanceMethod",
+      "short_name": "anInstanceMethod",
+      "kind": 16,
+      "storage": 0,
+      "declarations": [{
+          "spelling": "3:11-3:27",
+          "extent": "3:3-3:28",
+          "content": "- (void)anInstanceMethod;",
+          "param_spellings": [
