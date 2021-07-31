a641954eb3a800d1dce8961c3113313274386e96
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-07-31 09:22:27 +0000

    Ver bump

--- index_tests/templates/implicit_variable_instantiation.cc
+++ index_tests/templates/implicit_variable_instantiation.cc
@@ -6,4 +6,33 @@ namespace ns {
     static constexpr VarType static_var = (VarType)0x0;
   };
 
-  
+  template<typename _>
+  const typename VarType Holder<_>::static_var;
+
+
+  int Foo = Holder<int>::static_var;
+  int Foo2 = Holder<int>::static_var;
+}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [],
+  "usr2type": [{
+      "usr": 53,
+      "detailed_name": "",
+      "qual_name_offset": 0,
+      "short_name": "",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 0,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [12898699035586282159, 9008550860229740818],
+      "uses":
