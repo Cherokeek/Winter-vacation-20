7a2dc87fe1860338521c14d4e58c12eaf778a789
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-08-29 08:59:21 +0000

    "shenanigans-manager@0.2.16

--- index_tests/templates/template_type_usage_folded_into_one.cc
+++ index_tests/templates/template_type_usage_folded_into_one.cc
@@ -22,4 +22,26 @@ OUTPUT:
       "vars": [],
       "alias_of": 0,
       "kind": 5,
-      "parent_kind": 
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [16721564935990383768, 12028309045033782423],
+      "uses": ["4:1-4:4|4|-1", "5:1-5:4|4|-1"]
+    }],
+  "usr2var": [{
+      "usr": 12028309045033782423,
+      "detailed_name": "Foo<bool> b",
+      "qual_name_offset": 10,
+      "short_name": "b",
+      "spell": "5:11-5:12|5:1-5:12|2|-1",
+      "type": 10528472276654770367,
+      "kind": 13,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "uses": []
+    }, {
+      "usr": 16721564935990383768,
+      "detailed_name": "Foo<int> a",
+      "qual_name_offset": 9,
+     
