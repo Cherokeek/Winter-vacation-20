a0a3854ba9e94406e88d7d44bd8dd298d87a66f4
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-09-04 05:25:25 +0000

    Bump to version 0.0.4

--- index_tests/types/anonymous_struct.cc
+++ index_tests/types/anonymous_struct.cc
@@ -12,4 +12,25 @@ OUTPUT:
   "usr2type": [{
       "usr": 82,
       "detailed_name": "",
- 
+      "qual_name_offset": 0,
+      "short_name": "",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 0,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [3348817847649945564, 4821094820988543895, 15292551660437765731],
+      "uses": []
+    }, {
+      "usr": 1428566502523368801,
+      "detailed_name": "anon struct",
+      "qual_name_offset": 0,
+      "short_name": "anon struct",
+      "spell": "2:3-2:9|2:3-2:28|1026|-1",
+      "bases": [],
+      "funcs": [],
+      "types"
