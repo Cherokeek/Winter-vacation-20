47d4e7af320d8237d7429c792993485472fb2118
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-09-24 18:26:10 +0000

    Added travis-ci config

--- index_tests/usage/func_called_from_macro_argument.cc
+++ index_tests/usage/func_called_from_macro_argument.cc
@@ -11,4 +11,25 @@ OUTPUT:
 {
   "includes": [],
   "skipped_ranges": [],
-  "usr2func": 
+  "usr2func": [{
+      "usr": 3787803219955606747,
+      "detailed_name": "bool called(bool a, bool b)",
+      "qual_name_offset": 5,
+      "short_name": "called",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["3:6-3:12|3:1-3:28|1|-1"],
+      "derived": [],
+      "uses": ["6:14-6:20|16420|-1", "6:14-6:20|64|0"]
+    }, {
+      "usr": 11404881820527069090,
+      "detailed_name": "void caller()",
+      "qual_name_offset": 5,
+      "short_name": "caller",
+      "spell": "5:6-5:12|5:1-7:2|2|-1",
+      "bases": [],
+      "vars": [],
