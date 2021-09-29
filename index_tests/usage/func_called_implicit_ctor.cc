ac54b93bee7f64188bf1b92ac75543a1d3956ffe
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-09-29 16:01:58 +0000

    Bumping relnum

--- index_tests/usage/func_called_implicit_ctor.cc
+++ index_tests/usage/func_called_implicit_ctor.cc
@@ -26,4 +26,27 @@ OUTPUT:
       "parent_kind": 0,
       "storage": 0,
       "declarations": [],
-      "derived": []
+      "derived": [],
+      "uses": ["8:10-8:16|16420|-1"]
+    }, {
+      "usr": 10544127002917214589,
+      "detailed_name": "Wrapper::Wrapper(int i)",
+      "qual_name_offset": 0,
+      "short_name": "Wrapper",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 9,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["2:3-2:10|2:3-2:17|1025|-1"],
+      "derived": [],
+      "uses": ["8:10-8:16|16676|-1"]
+    }, {
+      "usr": 11404881820527069090,
+      "detailed_name": "Wrapper caller()",
+      "qual_name_offset": 8,
+      "short_name": "caller",
+      "spell": "7:9-7:15|7:1-9:2|2|-1",
+      "bases": [],
+      "vars": []
