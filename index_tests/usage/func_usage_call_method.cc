127ea33adc094579c8df1a916a0040d6c118aa7e
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-12-13 19:10:33 +0000

    Update Status.md

--- index_tests/usage/func_usage_call_method.cc
+++ index_tests/usage/func_usage_call_method.cc
@@ -47,4 +47,25 @@ OUTPUT:
       "detailed_name": "struct Foo {}",
       "qual_name_offset": 7,
       "short_name": "Foo",
-      "spell
+      "spell": "1:8-1:11|1:1-3:2|2|-1",
+      "bases": [],
+      "funcs": [18417145003926999463],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [14045150712868309451],
+      "uses": ["6:3-6:6|4|-1"]
+    }],
+  "usr2var": [{
+      "usr": 14045150712868309451,
+      "detailed_name": "Foo *f",
+      "qual_name_offset": 5,
+      "short_name": "f",
+      "hover": "Foo *f = nullptr",
+      "spell": "6:8-6:9|6:3-6:19|2|-1",
+      "type": 15041163540773201510,
+      "kind": 1
