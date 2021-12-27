fd012c23855a8dc85b1907959873982f8fecbc25
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2021-12-27 20:34:04 +0000

    reduced logging

--- index_tests/usage/func_usage_forward_decl_method.cc
+++ index_tests/usage/func_usage_forward_decl_method.cc
@@ -11,4 +11,27 @@ OUTPUT:
 {
   "includes": [],
   "skipped_ranges": [],
-  "
+  "usr2func": [{
+      "usr": 6767773193109753523,
+      "detailed_name": "void usage()",
+      "qual_name_offset": 5,
+      "short_name": "usage",
+      "spell": "5:6-5:11|5:1-8:2|2|-1",
+      "bases": [],
+      "vars": [16229832321010999607],
+      "callees": ["7:6-7:9|17922201480358737771|3|16420"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 17922201480358737771,
+      "detailed_name": "void Foo::foo()",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      
