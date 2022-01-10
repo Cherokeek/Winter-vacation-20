6c3778a4877d6cd50d43e100a925d8d62d925435
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-01-10 18:11:13 +0000

    Update .gitignore.

--- index_tests/usage/type_usage_as_template_parameter_complex.cc
+++ index_tests/usage/type_usage_as_template_parameter_complex.cc
@@ -1,1 +1,33 @@
-template<typename T, 
+template<typename T, typename B>
+class unique_ptr;
+
+struct S1;
+struct S2;
+
+#if false
+VarDecl f
+  TemplateRef unique_ptr
+  TemplateRef unique_ptr
+  TypeRef struct S1
+  TypeRef struct S2
+  TypeRef struct S2
+#endif
+extern unique_ptr<unique_ptr<S1, S2>, S2> f;
+
+#if false
+FunctionDecl as_return_type
+  TemplateRef unique_ptr
+  TemplateRef unique_ptr
+  TypeRef struct S1
+  TypeRef struct S2
+  TypeRef struct S2
+  ParmDecl
+    TemplateRef unique_ptr
+    TypeRef struct S1
+    TypeRef struct S2
+  CompoundStmt
+    ReturnStmt
+      UnexposedExpr
+        CXXNullPtrLiteralExpr
+#endif
+unique_ptr<unique_ptr<S1, S2>, S2>* as_ret
