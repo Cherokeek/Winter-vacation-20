786057cb0adce91e32b759355ad674c7cf1e696f
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-01-19 21:38:49 +0000

    Test coverage increased.

--- index_tests/usage/type_usage_as_template_parameter_complex.cc
+++ index_tests/usage/type_usage_as_template_parameter_complex.cc
@@ -30,4 +30,36 @@ FunctionDecl as_return_type
       UnexposedExpr
         CXXNullPtrLiteralExpr
 #endif
-unique_ptr<unique_ptr<S1, S2>, S2>* as_ret
+unique_ptr<unique_ptr<S1, S2>, S2>* as_return_type(unique_ptr<S1, S2>*) { return nullptr; }
+
+#if false
+FunctionDecl no_return_type
+  ParmDecl
+  CompoundStmt
+#endif
+void no_return_type(int) {}
+
+#if false
+FunctionDecl empty
+  CompoundStmt
+    DeclStmt
+      VarDecl local
+        TemplateRef unique_ptr
+        TemplateRef unique_ptr
+        TypeRef struct S1
+        TypeRef struct S2
+        TypeRef struct S2
+#endif
+void empty() {
+  unique_ptr<unique_ptr<S1, S2>, S2>* local;
+}
+
+#if false
+ClassDecl Foo
+  CXXMethod foo
+    TemplateRef unique_ptr
+    TypeRef struct S1
+    TypeRef struct S2
+#endif
+class Foo {
+  unique_pt
