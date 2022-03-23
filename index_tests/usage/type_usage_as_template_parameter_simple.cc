cd12b3cf83ace1edd03bd1152de574cccb328e1a
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-03-23 19:44:43 +0000

    v0.8.2

--- index_tests/usage/type_usage_as_template_parameter_simple.cc
+++ index_tests/usage/type_usage_as_template_parameter_simple.cc
@@ -21,4 +21,28 @@ OUTPUT:
       "funcs": [],
       "types": [],
       "vars": [],
-      "alias_of": 
+      "alias_of": 0,
+      "kind": 5,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [3398408600781120939],
+      "uses": ["6:8-6:18|4|-1"]
+    }, {
+      "usr": 4750332761459066907,
+      "detailed_name": "struct S",
+      "qual_name_offset": 7,
+      "short_name": "S",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": ["4:8-4:9|4:1-4:9|1|-1"],
+      "derived": [],
+      "instances": [],
+      "uses": ["6:19-6:20|4|-1"]
+    }],
+  
