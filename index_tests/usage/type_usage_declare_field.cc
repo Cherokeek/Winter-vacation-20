d85ebbfd6741dcd5ce57139d6a5c0a61de9741a2
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-04-25 14:43:00 +0000

    full commit

--- index_tests/usage/type_usage_declare_field.cc
+++ index_tests/usage/type_usage_declare_field.cc
@@ -12,4 +12,24 @@ OUTPUT:
   "includes": [],
   "skipped_ranges": [],
   "usr2func": [],
-  "usr
+  "usr2type": [{
+      "usr": 8508299082070213750,
+      "detailed_name": "struct ImplementedType {}",
+      "qual_name_offset": 7,
+      "short_name": "ImplementedType",
+      "spell": "2:8-2:23|2:1-2:26|2|-1",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [14727441168849658842],
+      "uses": ["6:3-6:18|4|-1"]
+    }, {
+      "usr": 13749354388332789217,
+      "detailed_name": "struct ForwardType",
+      "qual_name_offse
