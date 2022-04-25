9f6806e0e8987096cd47df789b81ed8ed8f8c17c
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-04-25 21:18:29 +0000

    add change

--- index_tests/usage/type_usage_declare_field.cc
+++ index_tests/usage/type_usage_declare_field.cc
@@ -32,4 +32,27 @@ OUTPUT:
     }, {
       "usr": 13749354388332789217,
       "detailed_name": "struct ForwardType",
-      "qual_name_offse
+      "qual_name_offset": 7,
+      "short_name": "ForwardType",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": ["1:8-1:19|1:1-1:19|1|-1"],
+      "derived": [],
+      "instances": [14314859014962085433],
+      "uses": ["5:3-5:14|4|-1"]
+    }, {
+      "usr": 15041163540773201510,
+      "detailed_name": "struct Foo {}",
+      "qual_name_offset": 7,
+      "short_name": "Foo",
+      "spell": "4:8-4:11|4:1-7:2|2|-1",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [{
+          
