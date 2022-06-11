ec282ac623beb57e7b399ed91f87231fdf1962b6
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-06-11 17:07:16 +0000

    Version 1.5

--- index_tests/usage/type_usage_declare_local.cc
+++ index_tests/usage/type_usage_declare_local.cc
@@ -13,4 +13,26 @@ OUTPUT:
   "skipped_ranges": [],
   "usr2func": [{
       "usr": 4654328188330986029,
-      "detailed_name": "vo
+      "detailed_name": "void Foo()",
+      "qual_name_offset": 5,
+      "short_name": "Foo",
+      "spell": "4:6-4:9|4:1-7:2|2|-1",
+      "bases": [],
+      "vars": [16374832544037266261, 2580122838476012357],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 8508299082070213750,
+      "detailed_name": "struct ImplementedType {}",
+      "qual_name_offset": 7,
+      "short_name": "ImplementedType",
+      "spell": "2:8-2:23|2:1-2:26|2|-1",
+      "bases": [],
+      "funcs": [],
+      "types": 
