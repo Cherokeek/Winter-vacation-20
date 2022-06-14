5a585d9a195dddc04aff4d1fc8509b64b19fcb4b
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-06-14 22:49:19 +0000

    indents

--- index_tests/usage/type_usage_declare_param.cc
+++ index_tests/usage/type_usage_declare_param.cc
@@ -16,4 +16,27 @@ OUTPUT:
       "spell": "4:6-4:9|4:1-4:47|2|-1",
       "bases": [],
       "vars": [13058491096576226774, 11055777568039014776],
-      "ca
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 8508299082070213750,
+      "detailed_name": "struct ImplementedType {}",
+      "qual_name_offset": 7,
+      "short_name": "ImplementedType",
+      "spell": "2:8-2:23|2:1-2:26|2|-1",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": [],
+      "derived": [],
+      "instances": [11055777568039014776
