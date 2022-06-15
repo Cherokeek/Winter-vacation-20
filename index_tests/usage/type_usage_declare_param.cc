3cfb00855eace115e94da4988e9594fbf9528eeb
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-06-15 15:29:45 +0000

    Version update

--- index_tests/usage/type_usage_declare_param.cc
+++ index_tests/usage/type_usage_declare_param.cc
@@ -39,4 +39,27 @@ OUTPUT:
       "parent_kind": 0,
       "declarations": [],
       "derived": [],
-      "instances": [11055777568039014776
+      "instances": [11055777568039014776],
+      "uses": ["4:26-4:41|4|-1"]
+    }, {
+      "usr": 13749354388332789217,
+      "detailed_name": "struct ForwardType",
+      "qual_name_offset": 7,
+      "short_name": "ForwardType",
+      "bases": [],
+      "funcs": [],
+      "types": [],
+      "vars": [],
+      "alias_of": 0,
+      "kind": 23,
+      "parent_kind": 0,
+      "declarations": ["1:8-1:19|1:1-1:19|1|-1"],
+      "derived": [],
+      "instances": [13058491096576226774],
+      "uses": ["4:10-4:21|4|-1"]
+    }],
+  "usr2var": [{
+      "usr": 11055777568039014776,
+      "detailed_name": "ImplementedType a",
+      "qual_name_offset": 16,
+   
