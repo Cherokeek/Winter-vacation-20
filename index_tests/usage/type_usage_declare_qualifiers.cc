0ea1bbb605cb10cf6b007792af358b2cf9c45280
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-08-06 19:55:13 +0000

    Bump version to 1.7.1

--- index_tests/usage/type_usage_declare_qualifiers.cc
+++ index_tests/usage/type_usage_declare_qualifiers.cc
@@ -13,4 +13,22 @@ OUTPUT:
   "skipped_ranges": [],
   "usr2func": [{
       "usr": 16858540520096802573,
-      "
+      "detailed_name": "void foo(Type &a0, const Type &a1)",
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "3:6-3:9|3:1-8:2|2|-1",
+      "bases": [],
+      "vars": [7997456978847868736, 17228576662112939520, 15429032129697337561, 6081981442495435784, 5004072032239834773, 14939253431683105646],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [{
+      "usr": 13487927231218873822,
+      "detailed_name": "struct Type {}",
+      "qual_name_offset": 7,
+      "short_na
