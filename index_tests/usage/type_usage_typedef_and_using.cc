a4c8a7fa52f9e80d8acfd8abdeda823b527542c0
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-08-30 22:52:07 +0000

    initialize commit

--- index_tests/usage/type_usage_typedef_and_using.cc
+++ index_tests/usage/type_usage_typedef_and_using.cc
@@ -1,3 +1,32 @@
 struct Foo;
 using Foo1 = Foo*;
-ty
+typedef Foo Foo2;
+using Foo3 = Foo1;
+using Foo4 = int;
+
+void accept(Foo*) {}
+void accept1(Foo1*) {}
+void accept2(Foo2*) {}
+void accept3(Foo3*) {}
+
+/*
+OUTPUT:
+{
+  "includes": [],
+  "skipped_ranges": [],
+  "usr2func": [{
+      "usr": 558620830317390922,
+      "detailed_name": "void accept1(Foo1 *)",
+      "qual_name_offset": 5,
+      "short_name": "accept1",
+      "spell": "8:6-8:13|8:1-8:23|2|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, 
