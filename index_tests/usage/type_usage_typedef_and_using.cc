fed46d3ede2345d85e52659fdb9e55af877e3da4
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-09-03 10:52:09 +0000

    Update .swift-version

--- index_tests/usage/type_usage_typedef_and_using.cc
+++ index_tests/usage/type_usage_typedef_and_using.cc
@@ -29,4 +29,28 @@ OUTPUT:
       "declarations": [],
       "derived": [],
       "uses": []
-    }, 
+    }, {
+      "usr": 9119341505144503905,
+      "detailed_name": "void accept(Foo *)",
+      "qual_name_offset": 5,
+      "short_name": "accept",
+      "spell": "7:6-7:12|7:1-7:21|2|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 10523262907746124479,
+      "detailed_name": "void accept2(Foo2 *)",
+      "qual_name_offset": 5,
+      "short_name": "accept2",
+      "spell": "9:6-9:13|9:1-9:23|2|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 1
