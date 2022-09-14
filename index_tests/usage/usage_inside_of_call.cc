4d37ce0c0f2b0bb31b394073d69d1f1bb411d6b0
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-09-14 05:38:37 +0000

    Delete post.html

--- index_tests/usage/usage_inside_of_call.cc
+++ index_tests/usage/usage_inside_of_call.cc
@@ -22,4 +22,25 @@ OUTPUT:
   "usr2func": [{
       "usr": 4259594751088586730,
       "detailed_name": "void foo()",
-      "qual_name_offset"
+      "qual_name_offset": 5,
+      "short_name": "foo",
+      "spell": "12:6-12:9|12:1-15:2|2|-1",
+      "bases": [],
+      "vars": [8039186520399841081],
+      "callees": ["14:3-14:9|18319417758892371313|3|16420", "14:14-14:17|11404602816585117695|3|16420"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 11404602816585117695,
+      "detailed_name": "int gen()",
+      "qual_name_offset": 4,
+      "short_name": "gen",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+    
