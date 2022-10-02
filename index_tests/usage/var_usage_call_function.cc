036ed2897a41c048ab96ce61b38818fa341c2b26
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-10-02 11:46:24 +0000

    The beginning

--- index_tests/usage/var_usage_call_function.cc
+++ index_tests/usage/var_usage_call_function.cc
@@ -13,4 +13,24 @@ OUTPUT:
   "includes": [],
   "skipped_ranges": [],
   "usr2func": [{
-      "usr": 
+      "usr": 468307235068920063,
+      "detailed_name": "void called()",
+      "qual_name_offset": 5,
+      "short_name": "called",
+      "spell": "1:6-1:12|1:1-1:17|2|-1",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": ["4:13-4:19|132|-1", "7:3-7:9|16420|-1"]
+    }, {
+      "usr": 11404881820527069090,
+      "detailed_name": "void caller()",
+      "qual_name_offset": 5,
+      "short_name": "caller",
+      "spell": "3:6-3:12|3:1-8:2|2|-1",
+   
