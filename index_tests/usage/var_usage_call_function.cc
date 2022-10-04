0c57b07ed51e1e5b2633109422311336241a1b5f
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-10-04 07:18:08 +0000

    Add pages

--- index_tests/usage/var_usage_call_function.cc
+++ index_tests/usage/var_usage_call_function.cc
@@ -33,4 +33,24 @@ OUTPUT:
       "qual_name_offset": 5,
       "short_name": "caller",
       "spell": "3:6-3:12|3:1-8:2|2|-1",
-   
+      "bases": [],
+      "vars": [9121974011454213596],
+      "callees": ["4:13-4:19|468307235068920063|3|132", "4:13-4:19|468307235068920063|3|132", "7:3-7:9|468307235068920063|3|16420"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }],
+  "usr2type": [],
+  "usr2var": [{
+      "usr": 9121974011454213596,
+      "detailed_name": "void (*)() x",
+      "qual_name_offset": 11,
+      "short_name": "x",
+      "hover": "void (*)() x = &called",
+      "spell": "4:8-4:9|4:3-4:19|2|-1",
+      "type": 0,
+      "kind": 13,
+      "parent_kind"
