9e92afe403d36640a2beb09935d18d5bd34202d5
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-10-11 18:05:14 +0000

    More todos

--- index_tests/usage/var_usage_class_member_static.cc
+++ index_tests/usage/var_usage_class_member_static.cc
@@ -20,4 +20,28 @@ OUTPUT:
       "short_name": "foo",
       "spell": "7:6-7:9|7:1-9:2|2|-1",
       "bases": [],
-      "
+      "vars": [],
+      "callees": ["8:3-8:9|17175780305784503374|3|16420"],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": [],
+      "derived": [],
+      "uses": []
+    }, {
+      "usr": 17175780305784503374,
+      "detailed_name": "void accept(int)",
+      "qual_name_offset": 5,
+      "short_name": "accept",
+      "bases": [],
+      "vars": [],
+      "callees": [],
+      "kind": 12,
+      "parent_kind": 0,
+      "storage": 0,
+      "declarations": ["5:6-5:12|5:1-5:17|1|-1"],
+      "derived": [],
+      "uses": ["8:3-8:9|16420|-1"]
+    }],
+  "usr2type": [{
+      "usr
