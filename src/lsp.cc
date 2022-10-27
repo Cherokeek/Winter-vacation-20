ab2d716845a8133434457b9987bd6adbe65db503
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-10-27 07:43:06 +0000

    Update master for stable/xena

--- src/lsp.cc
+++ src/lsp.cc
@@ -17,4 +17,29 @@ void reflect(JsonReader &vis, RequestId &v) {
     v.value = std::to_string(int(vis.m->GetInt64()));
   } else if (vis.m->IsInt()) {
     v.type = RequestId::kInt;
-    v.value = std::to_st
+    v.value = std::to_string(vis.m->GetInt());
+  } else if (vis.m->IsString()) {
+    v.type = RequestId::kString;
+    v.value = vis.m->GetString();
+  } else {
+    v.type = RequestId::kNone;
+    v.value.clear();
+  }
+}
+
+void reflect(JsonWriter &visitor, RequestId &value) {
+  switch (value.type) {
+  case RequestId::kNone:
+    visitor.null_();
+    break;
+  case RequestId::kInt:
+    visitor.int64(atoll(value.value.c_str()));
+    break;
+  case RequestId::kString:
+    visitor.string(value.value.c_str(), value.value.size());
+    break;
+  }
+}
+
+DocumentUri DocumentUri::fromPath(const std::string &path) {
+  DocumentU
