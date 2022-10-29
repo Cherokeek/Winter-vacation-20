399f41b14033a14b2a6fe5ddebc6d1d4a75fd601
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-10-29 14:45:36 +0000

    update fix bug

--- src/main.cc
+++ src/main.cc
@@ -14,4 +14,29 @@
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/Process.h>
 #include <llvm/Support/Program.h>
-#include <llvm/Suppo
+#include <llvm/Support/Signals.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace ccls;
+using namespace llvm;
+using namespace llvm::cl;
+
+namespace ccls {
+std::vector<std::string> g_init_options;
+}
+
+namespace {
+OptionCategory C("ccls options");
+
+opt<bool> opt_help("h", desc("Alias for -help"), cat(C));
+opt<int> opt_verbose("v", desc("verbosity, from -3 (fatal) to 2 (verbose)"),
+                     init(0), cat(C));
+opt<std::string> opt_test_index("test-index", ValueOptional, i
