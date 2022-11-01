8d3cb4baaa78719834ea0211871cb7b37df5ccdf
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-11-01 13:36:48 +0000

    moved things around

--- src/message_handler.hh
+++ src/message_handler.hh
@@ -80,4 +80,34 @@ enum class CompletionTriggerKind {
   TriggerCharacter = 2,
   TriggerForIncompleteCompletions = 3,
 };
-struct Completio
+struct CompletionContext {
+  CompletionTriggerKind triggerKind = CompletionTriggerKind::Invoked;
+  std::optional<std::string> triggerCharacter;
+};
+struct CompletionParam : TextDocumentPositionParam {
+  CompletionContext context;
+};
+enum class CompletionItemKind {
+  Text = 1,
+  Method = 2,
+  Function = 3,
+  Constructor = 4,
+  Field = 5,
+  Variable = 6,
+  Class = 7,
+  Interface = 8,
+  Module = 9,
+  Property = 10,
+  Unit = 11,
+  Value = 12,
+  Enum = 13,
+  Keyword = 14,
+  Snippet = 15,
+  Color = 16,
+  File = 17,
+  Reference = 18,
+  Folder = 19,
+  EnumMember = 20,
+  Constant = 21,
+  Struct = 22,
+  Even
