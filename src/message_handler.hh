fd9bbcb9e670847c0794ccc700cd9495975b2ae8
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-11-01 06:12:04 +0000

    docblock fixes

--- src/message_handler.hh
+++ src/message_handler.hh
@@ -1,2 +1,31 @@
 // Copyright 2017-2018 ccls Authors
-// S
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "lsp.hh"
+#include "query.hh"
+
+#include <functional>
+#include <memory>
+#include <optional>
+#include <vector>
+
+namespace ccls {
+struct SemaManager;
+struct VFS;
+struct IncludeComplete;
+struct Project;
+struct WorkingFile;
+struct WorkingFiles;
+
+namespace pipeline {
+void reply(const RequestId &id, const std::function<void(JsonWriter &)> &fn);
+void replyError(const RequestId &id,
+                const std::function<void(JsonWriter &)> &fn);
+} // namespace pipeline
+
+struct CodeActionParam {
+  TextDocumentIdentifier textDocument;
+  lsRange range;
+  str
