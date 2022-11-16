f5079af2833fa9fed61b6fe4c002200e27cd0e1d
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-11-16 20:12:39 +0000

    connector: use nlmsg_len() to check message length

--- src/messages/ccls_inheritance.cc
+++ src/messages/ccls_inheritance.cc
@@ -11,4 +11,26 @@
 namespace ccls {
 namespace {
 struct Param : TextDocumentPositionParam {
-  // If id+kind are specified, expand a node; otherwise te
+  // If id+kind are specified, expand a node; otherwise textDocument+position
+  // should be specified for building the root and |levels| of nodes below.
+  Usr usr;
+  std::string id;
+  Kind kind = Kind::Invalid;
+
+  // true: derived classes/functions; false: base classes/functions
+  bool derived = false;
+  bool qualified = true;
+  int levels = 1;
+  bool hierarchy = false;
+};
+
+REFLECT_STRUCT(Param, textDocument, position, id, kind, derived, qualified,
+               levels, hierarchy);
+
+struct Out_cclsInheritance {
+  Usr usr;
+  std::string id;
+  Kind kind;
+  std::string_view name;
+  Location location;
+  // For unexpanded nodes, this is an upper 
