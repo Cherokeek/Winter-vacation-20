dfb39e860185729779d824148eabd023583cc0bf
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-11-19 19:00:41 +0000

    Testing..

--- src/messages/ccls_navigate.cc
+++ src/messages/ccls_navigate.cc
@@ -4,4 +4,22 @@
 #include "message_handler.hh"
 #include "query.hh"
 
-namespa
+namespace ccls {
+namespace {
+struct Param {
+  TextDocumentIdentifier textDocument;
+  Position position;
+  std::string direction;
+};
+REFLECT_STRUCT(Param, textDocument, position, direction);
+
+Maybe<Range> findParent(QueryFile *file, Pos pos) {
+  Maybe<Range> parent;
+  for (auto [sym, refcnt] : file->symbol2refcnt)
+    if (refcnt > 0 && sym.extent.valid() && sym.extent.start <= pos &&
+        pos < sym.extent.end &&
+        (!parent || (parent->start == sym.extent.start
+                         ? parent->end < sym.extent.end
+                         : parent->start < sym.extent.start)))
+      parent = sym.extent;
+  
