aac8ad2493f69d26ec23914ac2b9a2f9af01976c
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-12-04 17:54:02 +0000

    fix a minor bug

--- src/messages/textDocument_code.cc
+++ src/messages/textDocument_code.cc
@@ -23,4 +23,18 @@ REFLECT_STRUCT(CodeAction, title, kind, edit);
 } // namespace
 void MessageHandler::textDocument_codeAction(CodeActionParam &param,
                                              ReplyOnce &reply) {
-  WorkingFi
+  WorkingFile *wf = findOrFail(param.textDocument.uri.getPath(), reply).second;
+  if (!wf)
+    return;
+  std::vector<CodeAction> result;
+  std::vector<Diagnostic> diagnostics;
+  wfiles->withLock([&]() { diagnostics = wf->diagnostics; });
+  for (Diagnostic &diag : diagnostics)
+    if (diag.fixits_.size() &&
+        (param.range.intersects(diag.range) ||
+         llvm::any_of(diag.fixits_, [&](const TextEdit &edit) {
+           return param.range.intersects(edit.range);
+         }))) {
+      CodeAction &cmd = result.emplace_back();
+      cmd.title = "FixIt: " + diag.message;
+      auto &edit =
