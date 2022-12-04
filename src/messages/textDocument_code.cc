2dce944cb65c18f3e2aff9876c3f767dea868022
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-12-04 08:47:49 +0000

    automatically publishing and installing (PyroFactory)

--- src/messages/textDocument_code.cc
+++ src/messages/textDocument_code.cc
@@ -1,1 +1,26 @@
-// Copyright
+// Copyright 2017-2018 ccls Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include "message_handler.hh"
+#include "pipeline.hh"
+#include "query.hh"
+
+#include <llvm/Support/FormatVariadic.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+
+#include <unordered_set>
+
+namespace ccls {
+namespace {
+struct CodeAction {
+  std::string title;
+  const char *kind = "quickfix";
+  WorkspaceEdit edit;
+};
+REFLECT_STRUCT(CodeAction, title, kind, edit);
+} // namespace
+void MessageHandler::textDocument_codeAction(CodeActionParam &param,
+                                             ReplyOnce &reply) {
+  WorkingFi
