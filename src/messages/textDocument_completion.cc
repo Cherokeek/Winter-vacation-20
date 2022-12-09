2b74e47b7d686156c2536769d12e3a6e97dde2d8
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-12-09 20:04:01 +0000

    fix compilation

--- src/messages/textDocument_completion.cc
+++ src/messages/textDocument_completion.cc
@@ -10,4 +10,30 @@
 #include "working_files.hh"
 
 #include <clang/Sema/CodeCompleteConsumer.h>
-#include <
+#include <clang/Sema/Sema.h>
+#include <llvm/ADT/Twine.h>
+
+#if LLVM_VERSION_MAJOR < 8
+#include <regex>
+#endif
+
+namespace ccls {
+using namespace clang;
+using namespace llvm;
+
+REFLECT_UNDERLYING(InsertTextFormat);
+REFLECT_UNDERLYING(CompletionItemKind);
+
+void reflect(JsonWriter &vis, CompletionItem &v) {
+  reflectMemberStart(vis);
+  REFLECT_MEMBER(label);
+  REFLECT_MEMBER(kind);
+  REFLECT_MEMBER(detail);
+  if (v.documentation.size())
+    REFLECT_MEMBER(documentation);
+  REFLECT_MEMBER(sortText);
+  if (v.filterText.size())
+    REFLECT_MEMBER(filterText);
+  REFLECT_MEMBER(insertTextFormat);
+  REFLECT_MEMBER(textEdit);
+  if (v.ad
