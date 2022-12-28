f165ed5c0965acca09d59ef02181474f3fe3e1ff
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-12-28 05:44:47 +0000

    Updated project settings.

--- src/messages/textDocument_did.cc
+++ src/messages/textDocument_did.cc
@@ -9,4 +9,19 @@
 #include "working_files.hh"
 
 namespace ccls {
-void MessageHandler::textDocum
+void MessageHandler::textDocument_didChange(TextDocumentDidChangeParam &param) {
+  std::string path = param.textDocument.uri.getPath();
+  wfiles->onChange(param);
+  if (g_config->index.onChange)
+    pipeline::index(path, {}, IndexMode::OnChange, true);
+  manager->onView(path);
+  if (g_config->diagnostics.onChange >= 0)
+    manager->scheduleDiag(path, g_config->diagnostics.onChange);
+}
+
+void MessageHandler::textDocument_didClose(TextDocumentParam &param) {
+  std::string path = param.textDocument.uri.getPath();
+  wfiles->onClose(path);
+  manager->onClose(path);
+  pipeline::removeCache(path);
+}
