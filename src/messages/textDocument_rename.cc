50f6e8da51f2a83b687f4ceab847486e851fb709
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2022-12-29 22:50:52 +0000

    Update Decode

--- src/messages/textDocument_rename.cc
+++ src/messages/textDocument_rename.cc
@@ -19,4 +19,20 @@ WorkspaceEdit buildWorkspaceEdit(DB *db, WorkingFiles *wfiles, SymbolRef sym,
   std::unordered_map<int, std::unordered_set<Range>> edited;
 
   eachOccurrence(db, sym, true, [&](Use use) {
-    int file_id
+    int file_id = use.file_id;
+    QueryFile &file = db->files[file_id];
+    if (!file.def || !edited[file_id].insert(use.range).second)
+      return;
+    std::optional<Location> loc = getLsLocation(db, wfiles, use);
+    if (!loc)
+      return;
+
+    auto [it, inserted] = path2edit.try_emplace(file_id);
+    auto &edit = it->second.second;
+    if (inserted) {
+      const std::string &path = file.def->path;
+      edit.textDocument.uri = DocumentUri::fromPath(path);
+      if ((it->second.first = wfiles->getFile(path)))
+        edit.textDocument.version = it->second.first->version;
+    }
+    // TODO LoadIndexedContent if
