45f848e73bb784cf768ef455bea86eed7db3ae11
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-01-09 20:31:05 +0000

    RELEASE : Bumped version.

--- src/pipeline.cc
+++ src/pipeline.cc
@@ -381,4 +381,23 @@ bool indexer_Parse(SemaManager *completion, WorkingFiles *wfiles,
     if (LOG_V_ENABLED(1)) {
       msg += "\n ";
       for (const char *arg : entry.args)
-        (msg += ' ') += ar
+        (msg += ' ') += arg;
+    }
+    LOG_S(INFO) << std::string_view(msg.data(), msg.size());
+  }
+
+  for (std::unique_ptr<IndexFile> &curr : indexes) {
+    std::string path = curr->path;
+    if (!matcher.matches(path)) {
+      LOG_IF_S(INFO, loud) << "skip index for " << path;
+      continue;
+    }
+
+    if (!deleted)
+      LOG_IF_S(INFO, loud) << "store index for " << path
+                           << " (delta: " << !!prev << ")";
+    {
+      std::lock_guard lock(getFileMutex(path));
+      int loaded = vfs->loaded(path), retain = g_config->cache.retainInMemory;
+      if (loaded)
+        prev = rawC
