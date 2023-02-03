6d7fc749fcdf18de1e57524ef356573bb8a5c8a4
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-02-03 13:38:20 +0000

    improve help

--- src/position.cc
+++ src/position.cc
@@ -33,4 +33,28 @@ Range Range::fromString(const std::string &encoded) {
   Pos start, end;
   char *p = const_cast<char *>(encoded.c_str());
   start.line = uint16_t(strtoul(p, &p, 10) - 1);
-  assert(*p == ':
+  assert(*p == ':');
+  p++;
+  start.column = int16_t(strtol(p, &p, 10)) - 1;
+  assert(*p == '-');
+  p++;
+
+  end.line = uint16_t(strtoul(p, &p, 10) - 1);
+  assert(*p == ':');
+  p++;
+  end.column = int16_t(strtol(p, nullptr, 10)) - 1;
+  return {start, end};
+}
+
+bool Range::contains(int line, int column) const {
+  if (line > UINT16_MAX)
+    return false;
+  Pos p{(uint16_t)line, (int16_t)std::min<int>(column, INT16_MAX)};
+  return !(p < start) && p < end;
+}
+
+std::string Range::toString() {
+  char buf[99];
+  snprintf(buf, sizeof buf, "%d:%d-%d:%d", start.line + 1, start.column + 1,
+           end.line + 1, end.column + 1);
+  return buf;
