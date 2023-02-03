250b991494637b05fd5eb150fb47634010dedbf0
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-02-03 09:21:26 +0000

    add controller tests

--- src/position.cc
+++ src/position.cc
@@ -11,3 +11,26 @@
 #include <algorithm>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+namespace ccls {
+Pos Pos::fromString(const std::string &encoded) {
+  char *p = const_cast<char *>(encoded.c_str());
+  uint16_t line = uint16_t(strtoul(p, &p, 10) - 1);
+  assert(*p == ':');
+  p++;
+  int16_t column = int16_t(strtol(p, &p, 10)) - 1;
+  return {line, column};
+}
+
+std::string Pos::toString() {
+  char buf[99];
+  snprintf(buf, sizeof buf, "%d:%d", line + 1, column + 1);
+  return buf;
+}
+
+Range Range::fromString(const std::string &encoded) {
+  Pos start, end;
+  char *p = const_cast<char *>(encoded.c_str());
+  start.line = uint16_t(strtoul(p, &p, 10) - 1);
+  assert(*p == ':
