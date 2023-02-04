a83be3f6970c63a477c6669b81cba8ef6e1e8ddd
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-02-04 09:59:36 +0000

    Create new file

--- src/position.hh
+++ src/position.hh
@@ -6,4 +6,29 @@
 #include "utils.hh"
 
 #include <stdint.h>
-#include <s
+#include <string>
+
+namespace ccls {
+struct Pos {
+  uint16_t line = 0;
+  int16_t column = -1;
+
+  static Pos fromString(const std::string &encoded);
+
+  bool valid() const { return column >= 0; }
+  std::string toString();
+
+  // Compare two Positions and check if they are equal. Ignores the value of
+  // |interesting|.
+  bool operator==(const Pos &o) const {
+    return line == o.line && column == o.column;
+  }
+  bool operator<(const Pos &o) const {
+    if (line != o.line)
+      return line < o.line;
+    return column < o.column;
+  }
+  bool operator<=(const Pos &o) const { return !(o < *this); }
+};
+
+struct Range {
