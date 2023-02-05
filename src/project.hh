98c35c2746e6e924caad46a27f51c28b50dd92fa
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-02-05 20:26:02 +0000

    README.rst updated

--- src/project.hh
+++ src/project.hh
@@ -20,4 +20,23 @@ std::pair<LanguageId, bool> lookupExtension(std::string_view filename);
 struct Project {
   struct Entry {
     std::string root;
-    std::string directo
+    std::string directory;
+    std::string filename;
+    std::vector<const char *> args;
+    // If true, this entry is inferred and was not read from disk.
+    bool is_inferred = false;
+    // 0 unless coming from a compile_commands.json entry.
+    int compdb_size = 0;
+    int id = -1;
+  };
+
+  struct Folder {
+    std::string name;
+    std::unordered_map<std::string, int> search_dir2kind;
+    std::vector<Entry> entries;
+    std::unordered_map<std::string, int> path2entry_index;
+    std::unordered_map<std::string, std::vector<const char *>> dot_ccls;
+  };
+
+  std::mutex mtx;
+  std::unordered_map<std::string, 
