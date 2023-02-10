05f3ff45c3dd34cfc5418f7754355d0194654ad5
Cherokeek <104671622+Cherokeek@users.noreply.github.com>
2023-02-10 08:19:56 +0000

    Auto update latest refs.

--- src/test.cc
+++ src/test.cc
@@ -15,4 +15,27 @@
 #include <llvm/Config/llvm-config.h>
 
 #include <rapidjson/document.h>
-#include <rapidjson/prett
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+// The 'diff' utility is available and we can use dprintf(3).
+#if _POSIX_C_SOURCE >= 200809L
+#include <sys/wait.h>
+#include <unistd.h>
+#endif
+
+using namespace llvm;
+
+extern bool gTestOutputMode;
+
+namespace ccls {
+std::string toString(const rapidjson::Document &document) {
+  rapidjson::StringBuffer buffer;
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+  writer.SetFormatOptions(
+      rapidjson::Prett
